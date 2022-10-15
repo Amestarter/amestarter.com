925ca4728dbfe74fde1eed6090e651874fbcebb8
phoenixguild <shuaibshaaban@gmail.com>
2022-10-15 03:00:54 +0100

    updated the hamburger menu

--- NEW-SITE/script.js
+++ NEW-SITE/script.js
@@ -96,3 +96,19 @@ const disableAutoSlideOnHover = () => {
   } );
 }
 disableAutoSlideOnHover();
+
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links span");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
