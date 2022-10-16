79dcf6da8e41b4110375b5459acf6baff3902a56
phoenixguild <shuaibshaaban@gmail.com>
2022-10-16 17:28:01 +0100

    fixed alot!

--- NEW-SITE/menu.js
+++ NEW-SITE/menu.js
@@ -1,17 +1,26 @@
-let burger = document.getElementById('burger'),
-	 nav    = document.getElementById('main-nav');
-	//  slowmo = document.getElementById('slowmo');
+// let burger = document.getElementById('burger'),
+// 	 nav    = document.getElementById('main-nav');
+// 	//  slowmo = document.getElementById('slowmo');
 
-burger.addEventListener('click', function(e){
-	this.classList.toggle('is-open');
-	nav.classList.toggle('is-open');
+// burger.addEventListener('click', function(e){
+// 	this.classList.toggle('is-open');
+// 	nav.classList.toggle('is-open');
+// });
+const
+  burger = document.querySelector("#burger"), 
+  nav = document.querySelector("#main-nav"),  // Use ID, if you already use one.
+  a = document.querySelectorAll(".nav-lists"); // PS: Use the right selectors!
+
+burger.addEventListener("click", function(e) {
+  this.classList.toggle("is-open");
+  nav.classList.toggle("is-open");
+  document.body.classList.toggle("stop-scrolling");
+  
 });
 
+a.forEach(el => el.addEventListener("click", function(e) {
+  burger.classList.toggle("is-open");
+  document.body.classList.remove("stop-scrolling");
+  nav.classList.toggle("is-open");
+}));
 
-// const navToggle = document.querySelector('.nav-toggle')
-const navLink = document.querySelectorAll('.nav-lists')
-
-
-navLink.addEventListener('click', (e) => {
-    nav.classList.toggle('is-open');
-})
