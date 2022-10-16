ca25ee076e7a816c6666b3799fd46d30c47c2f59
phoenixguild <shuaibshaaban@gmail.com>
2022-10-16 10:54:58 +0100

    nav bar fixed!

--- NEW-SITE/script.js
+++ NEW-SITE/script.js
@@ -128,40 +128,6 @@ function isItemInView(item){
     );
 }
 
-function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-      if (isItemInView(items[i])) {
-        items[i].classList.add("show");
-      }
-    }
-  }
-
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
-
-
-  const progressBar = document.getElementById("progressbar");
-progressBar.style.height = 1 + "%";
-
-window.onscroll = () => {
-	const scroll = document.documentElement.scrollTop;
-	const height =
-		document.documentElement.scrollHeight - document.documentElement.clientHeight;
-	let scrolled = (scroll / height) * 100;
-
-	if (scrolled <= 1) {
-		progressBar.style.height = 1 + "%";
-	} else if (scrolled >= 2 && scrolled <= 99.9) {
-		progressBar.style.height = scrolled + "%";
-		progressBar.classList.remove("glow");
-	} else if (scrolled === 100) {
-		progressBar.style.height = scrolled + "%";
-		// 		Do something when reached 100% scroll
-		progressBar.classList.add("glow");
-	}
-};
 
 
 let toggles = document.getElementsByClassName('toggle');
