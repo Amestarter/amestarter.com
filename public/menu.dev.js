e53a3e5465ea9cb91d77b2f9c3e21e3435304ebe
phoenixguild <shuaibshaaban@gmail.com>
2022-10-23 17:37:52 +0100

    updated js

--- public/menu.dev.js
+++ public/menu.dev.js
@@ -5,13 +5,16 @@ var burger = document.querySelector("#burger"),
     hamburger = document.querySelector("#hamburger");
 a = document.querySelectorAll(".nav-lists");
 burger.addEventListener("click", function (e) {
-  mobileNav.classList.toggle("is-open"); // document.body.classList.toggle("stop-scrolling");
+  mobileNav.classList.toggle("is-open");
+  document.body.classList.toggle("stop-scrolling");
 });
 hamburger.addEventListener("click", function (e) {
-  mobileNav.classList.toggle("is-open"); // document.body.classList.toggle("stop-scrolling");
+  mobileNav.classList.toggle("is-open");
+  document.body.classList.toggle("stop-scrolling");
 });
 a.forEach(function (el) {
   return el.addEventListener("click", function (e) {
-    mobileNav.classList.toggle("is-open"); // document.body.classList.remove("stop-scrolling");
+    mobileNav.classList.toggle("is-open");
+    document.body.classList.remove("stop-scrolling");
   });
 });
