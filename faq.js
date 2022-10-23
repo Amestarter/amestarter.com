f0b1e07eeee86d1007f1029c00f43a87be0233c2
SteffQing <steveola23@gmail.com>
2022-10-23 01:02:43 +0100

    Quick updates ahead of stylings

--- faq.js
+++ faq.js
@@ -5,12 +5,12 @@ for (let i = 0; i < toggler.length; i++) {
   toggler[i].addEventListener("click", () => {
     if (parseInt(divContent[i].style.height) != divContent[i].scrollHeight) {
       divContent[i].style.height = divContent[i].scrollHeight + "px";
-      toggler[i].style.color = "#ce8a0d";
+      toggler[i].style.color = "#EC984F";
       iconElement[i].classList.remove("fa-plus");
       iconElement[i].classList.add("fa-minus");
     } else {
       divContent[i].style.height = "0px";
-      toggler[i].style.color = "#111130";
+      toggler[i].style.color = "#fff";
       iconElement[i].classList.remove("fa-minus");
       iconElement[i].classList.add("fa-plus");
     }
@@ -18,7 +18,7 @@ for (let i = 0; i < toggler.length; i++) {
     for (let j = 0; j < divContent.length; j++) {
       if (j !== i) {
         divContent[j].style.height = "0px";
-        toggler[j].style.color = "#111130";
+        toggler[j].style.color = "#fff";
         iconElement[j].classList.remove("fa-minus");
         iconElement[j].classList.add("fa-plus");
       }
